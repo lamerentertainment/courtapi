20d977ee1f9c38797735ef97a885df46e5b52b32
lamerentertainment <jonasachermann@mac.com>
2023-06-07 23:21:02 +0200

    transform_text endpoint geschaffen

--- courtapi/views.py
+++ courtapi/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from converters import als_aussage_formatieren
+
+def transform_text(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body.decode('utf-8'))
+            input_text = data.get('text', '')
+            transformed_text = input_text.upper()  # Example transformation: converting text to uppercase
+            response_data = {'transformed_text': transformed_text}
+            return JsonResponse(response_data)
+        except json.JSONDecodeError:
+            return JsonResponse({'error': 'Invalid JSON'}, status=400)
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=405)
