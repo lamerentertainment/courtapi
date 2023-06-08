f0bc039498af2646d84752aa4cc276887ac06428
lamerentertainment <jonasachermann@mac.com>
2023-06-08 22:32:58 +0200

    wep request on appliku

--- request_text.py
+++ request_text.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 url = 'http://localhost:8000/api/v1/transform_text'
+weburl = "https://courtapi.applikuapp.com/api/v1/transform_text"
 payload = {
     'text': """Ich weiss nicht, was ich sa-
 gen soll, es geht mir mies. Morgen werden wir auf den Mond ge-
@@ -16,7 +17,7 @@ json_payload = json.dumps(payload)
 headers = {'Content-Type': 'application/json'}
 
 # Send the POST request
-response = requests.post(url, data=json_payload, headers=headers)
+response = requests.post(weburl, data=json_payload, headers=headers)
 
 # Print the response
 print(response.json())
